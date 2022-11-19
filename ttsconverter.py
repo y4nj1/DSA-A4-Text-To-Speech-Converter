76ca14e859a154172651b4de3cc802edb53edb01
Gian Carlo Estrella <giancarlobestrella@iskolarngbayan.pup.edu.ph>
2022-11-19 11:12:15 +0800

    Updated main window with pictures

--- ttsconverter.py
+++ ttsconverter.py
@@ -10,6 +10,12 @@ from playsound import playsound
 root = Tk()
 root.title("Text to Speech Converter")
 root.geometry("1000x580+200+80")
-root.resizable(True, True)
+root.resizable(False, False)
 root.configure(bg = "#b5e48c")
+# root.mainloop()
+
+self_logo = PhotoImage(file="C:\\Users\\Estrella\\Desktop\\logo_1.png")
+Label(root, image = self_logo, bg="#b5e48c").place(x = 550, y = 260)
+logo_image = PhotoImage(file="C:\\Users\\Estrella\\Desktop\\logo_2.png")
+root.iconphoto(False, logo_image)
 root.mainloop()
